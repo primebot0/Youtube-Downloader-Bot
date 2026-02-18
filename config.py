153a1a27955bc7c35de9b340b3c0267c7d86c918
primebot0 <primevideo19970@gmail.com>
2026-02-18 14:57:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import  os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-APP_ID = int(os.environ.get("API_ID"))
-API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("8541024933:AAGRVAMxtQhs61fuh5BBUzvDxRhaXK5RGvQ")
+APP_ID = int(os.environ.get("9411723"))
+API_HASH = os.environ.get("30fa091455c0548d77dc254f0bb705b0")
 
 youtube_next_fetch = 0  # time in minute
 
